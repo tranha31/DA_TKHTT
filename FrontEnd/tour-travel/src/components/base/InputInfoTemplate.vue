ea966effe33d5f22c5c1513fe52658bfd3d69e69
hangvtt183524 <nanh.trang.140@gmail.com>
2022-06-07 01:41:18 +0700

    create Input for common

---
+++ FrontEnd/tour-travel/src/components/base/InputInfoTemplate.vue
@@ -0,0 +1,101 @@
+<template>
+  <div class="item-cbb">
+    <span>{{ item }}</span>
+    <div v-if="isEditing">
+      <input
+          :type="inputType"
+          v-model="input"
+          :placeholder="placeholder"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "InputInfoTemplate",
+  props: {
+    item: {
+      required: true
+    },
+    isEditing: {
+      type: Boolean,
+      default: false
+    },
+    visible: {
+      type: Boolean,
+      default: true
+    },
+    label: {
+      type: String,
+      default: ''
+    },
+    inputType: {
+      type: String,
+      default: 'text'
+    },
+    placeholder: {
+      type: String,
+      default: ''
+    },
+    isValid: {
+      type: Boolean,
+      default: true
+    },
+    mandatory: {
+      type: Boolean,
+      default: false
+    },
+    value: {
+      type: [String, Number],
+      default: ''
+    },
+    isOnlyAlpha: {
+      type: Boolean,
+      default: true
+    },
+    isOnlyNumeric: {
+      type: Boolean,
+      default: true
+    }
+  },
+  computed: {
+    valid() {
+      return this.isValid
+    },
+    input: {
+      get() {
+        if (this.isOnlyAlpha) {
+          let checkedText = this.value !== null ? this.value.replace(/[a-zA-Z]+/g, '') : this.value
+          return checkedText
+        } else if (this.isOnlyNumeric) {
+          let checkedText = this.value !== null ? this.value.replace(/[0-9]+/g, '') : this.value
+          return checkedText
+        }
+        return this.value
+      },
+      set(value) {
+        this.$emit('input', value)
+      }
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style>
+.item-cbb{
+  height: 32px;
+  padding: 16px 0px 6px 20px;
+  color: #01B075;
+  margin-bottom: 40px;
+}
+
+.item-cbb span {
+  font-size: 20px;
+}
+
+
+</style>
